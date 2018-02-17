113a6033ca838c12b4bb77fffd1a094b61c7e25b
Joanne Wardell <jawardell@valdosta.edu>
2018-02-17 10:24:19 -0500

    person updates

--- people.js
+++ people.js
@@ -1,3 +1,7 @@
+const TreeMap = require("treemap-js");
+
+
+
 class Person {
 	constructor(name, username, password, email) {
 		this.name = name; 
@@ -5,4 +9,32 @@ class Person {
 		this.password = password;
 		this.email = email;
 	}
+
+	parseTrash(trash) {
+		this.trash = trash;
+	}
+
+	parseWater(water) {
+		this.water = water;
+	}
 }
+
+
+function makePerson() {
+	var person = {
+		name = document.getElementById();//get name
+		email = document.getElementById();//get	email 
+		water = document.getElementById();//get water usage
+		trash = document.getElementById();//get trash made
+
+	};
+}
+
+
+var map = new TreeMap();
+
+
+
+
+
+map.set();
